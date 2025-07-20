f8028181536f346408da211d0d5e4a2ed4f9600e
Deekshitha-Shanmugam <nalinideekshitha@gmail.com>
2025-07-21 01:39:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
